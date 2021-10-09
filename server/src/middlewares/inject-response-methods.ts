c86e0eb4e9c441aaf1a3687da9923c35f9112e80
EmJee <mart-janroeleveld@outlook.com>
2021-10-09 10:48:05 +0200

    5xx error responses now have optional errors

--- server/src/middlewares/inject-response-methods.ts
+++ server/src/middlewares/inject-response-methods.ts
@@ -5,7 +5,13 @@ const injectResponseMethods = (
 	res: Response,
 	next: NextFunction
 ): void => {
-	res.success = (status, data) => res.json({ data }).status(status)
+	res.success = (status, data) => {
+		if (status === 204) {
+			return res.sendStatus(status)
+		}
+
+		return res.json({ data }).status(status)
+	}
 	res.clientError = (status, ...errors) =>
 		res
 			.json({
@@ -18,7 +24,23 @@ const injectResponseMethods = (
 				}),
 			})
 			.status(status)
-	res.serverError = status => res.sendStatus(status)
+	res.serverError = (status, ...errors) => {
+		if (errors) {
+			return res
+				.json({
+					errors: errors.map(err => {
+						if (err instanceof Error) {
+							return err.message
+						}
+
+						return err
+					}),
+				})
+				.status(status)
+		}
+
+		return res.sendStatus(status)
+	}
 
 	next()
 }
--- server/src/models/express/index.d.ts
+++ server/src/models/express/index.d.ts
@@ -2,14 +2,15 @@ declare namespace Express {
 	export interface Response {
 		success: (
 			status: import('../../utils/status-codes').SuccessfulHttpStatus,
-			data: any[] | { [key: string]: any }
+			data?: any[] | { [key: string]: any }
 		) => import('express').Response<any, Record<string, any>>
 		clientError: (
 			status: import('../../utils/status-codes').ClientErrorHttpStatus,
 			...errors: (string | Error)[]
 		) => import('express').Response<any, Record<string, any>>
 		serverError: (
-			status: import('../../utils/status-codes').ServerErrorHttpStatus
+			status: import('../../utils/status-codes').ServerErrorHttpStatus,
+			...errors: (string | Error)[]
 		) => import('express').Response<any, Record<string, any>>
 	}
 }
